290513957435871b669eb81087f016ab1bac722d
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2022-10-16 16:17:18 -0400

    Added optional filtering url params for getting activities per user

--- src/activities/activities.service.ts
+++ src/activities/activities.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateActivityDto } from './dto/create-activity.dto';
 import { UpdateActivityDto } from './dto/update-activity.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Activity, ActivityCategory, PrismaPromise, SignificanceLevel } from '@prisma/client';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Injectable()
 export class ActivitiesService {
@@ -15,6 +17,45 @@ export class ActivitiesService {
   findAll(userId: number) {
     // will need to check that the user making the request is either the professor or is on the MC
     return this.prisma.activity.findMany({ where: { userId: userId } });
+  }
+
+  findFilter(userId: number, category: ActivityCategory | undefined, significance: SignificanceLevel | undefined) {
+    let result:PrismaPromise<Activity[]>;
+
+    if(!significance) {
+      result = this.prisma.activity.findMany({ where: {userId: userId, category: category}})
+    }
+    else if(!category) {
+      result = this.prisma.activity.findMany({ where: {userId: userId, significance: significance}})
+    }
+    else if(!significance && !category) {
+      result = this.prisma.activity.findMany({ where: {userId: userId}})
+    }
+    else {
+      result = this.prisma.activity.findMany({ where: {userId: userId, category: category, significance: significance}})
+    }
+
+    return result;
+  }
+
+  find(activityCatetory: ActivityCategory | undefined, significanceLevel: SignificanceLevel | undefined) {
+    if(activityCatetory === undefined) {
+      // do stuff
+    }
+
+  }
+
+  findByCategory(activityCatetory: ActivityCategory) {
+    return this.prisma.activity.findMany({
+      where: { category: activityCatetory}
+    })
+  }
+
+  findByLevel(significanceLevel: SignificanceLevel) {
+    return this.prisma.activity.findMany({
+      where: { significance: significanceLevel}
+    })
+
   }
 
   findOne(id: number) {
