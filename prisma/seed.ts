967094c2584589fd86befdd1aaf3ad294f9b487e
diegohernand <diggs.a.hedz@gmail.com>
2023-01-28 15:31:53 -0500

    Removed AcademicYear table, changed Activity & Narrative tables by replacing academicYearId with semester. Updated the activity dto and edited the seed script to conform with new schema.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -121,11 +121,7 @@ async function createActivityData() {
           email: 'a@b.com',
         },
       },
-      academicYear: {
-        connect: {
-          id: 1,
-        },
-      },
+      semester: 'FALL',
       date: new Date(),
       name: 'Taught Course',
       description:
@@ -145,11 +141,7 @@ async function createActivityData() {
           email: 'a3@b.com',
         },
       },
-      academicYear: {
-        connect: {
-          id: 1,
-        },
-      },
+      semester: 'FALL',
       date: new Date('2020-11-20T21:23:57.736Z'),
       name: 'Client Project',
       description: 'completed the client design project for Autodesk',
@@ -168,11 +160,7 @@ async function createActivityData() {
           email: 'a6@b.com',
         },
       },
-      academicYear: {
-        connect: {
-          id: 1,
-        },
-      },
+     semester: 'FALL',
       date: new Date('2022-11-20T21:23:57.736Z'),
       name: 'Directed Study',
       description: 'Animation simulation using Houdini',
@@ -191,11 +179,7 @@ async function createActivityData() {
           email: 'a8@b.com',
         },
       },
-      academicYear: {
-        connect: {
-          id: 1,
-        },
-      },
+      semester:'SUMMER2',
       date: new Date('2022-08-20T21:23:57.736Z'),
       name: 'New Course',
       description: 'ARTG 5240',
@@ -215,11 +199,7 @@ async function createActivityData() {
           email: 'a@b.com',
         },
       },
-      academicYear: {
-        connect: {
-          id: 1,
-        },
-      },
+      semester: 'FALL',
       date: new Date('2022-10-20T21:23:57.736Z'),
       name: 'Field Trip',
       description: 'led a field trip to the MFA',
@@ -238,12 +218,8 @@ async function createActivityData() {
           email: 'a8@b.com',
         },
       },
-      academicYear: {
-        connect: {
-          id: 1,
-        },
-      },
-      date: new Date('2022-10-20T21:23:57.736Z'),
+      semester: 'SPRING',
+      date: new Date('2022-4-20T21:23:57.736Z'),
       name: 'Led a minor teaching activity',
       description: 'something that I will edit later',
       category: 'TEACHING',
@@ -261,12 +237,8 @@ async function createActivityData() {
           email: 'a8@b.com',
         },
       },
-      academicYear: {
-        connect: {
-          id: 1,
-        },
-      },
-      date: new Date('2022-10-20T21:23:57.736Z'),
+      semester: 'SUMMER1',
+      date: new Date('2022-6-20T21:23:57.736Z'),
       name: 'Cure cancer',
       description: 'where is my money?',
       category: 'RESEARCH',
@@ -349,14 +321,6 @@ async function createProfessorInfoData() {
 }
 
 async function main() {
-  await prisma.academicYear.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      startDate: new Date(),
-      endDate: new Date(),
-    },
-  });
   await createUserData();
   await createActivityData();
   await createProfessorInfoData();
